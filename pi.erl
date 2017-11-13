5787a1a82ef6d411e9e03c69c9f32a7da927b2d4
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-13 23:44:16 +0000

    Allow user to set decimal precision of calculate_pi

--- pi.erl
+++ pi.erl
@@ -1,20 +1,15 @@
 -module(pi).
 -export([calculate_pi/1]).
 
-% https://stackoverflow.com/a/27792212
-even(X) when X >= 0 -> (X band 1) == 0.
-
 % first parameter: number of rounds
-calculate_pi(A) ->
-calculate_pi(A, 4).
+calculate_pi(Precision) ->
+calculate_pi(0, 0, 1, Precision, null, 4).
+    
 
-% first parameter: number of rounds left
-% second parameter: latest calculated value of pi
-% third parameter: latest calculated value of pi rounded up to the 5th decimal
-% fourth parameter: before-latest calculated value of pi rounded up to the 5th decimal
-calculate_pi(0, A) -> A;
-calculate_pi(A, B) ->
-    case even(A) of
-        true -> calculate_pi(A-1, B + 4 * (1 / (1 + A * 2)));
-        false -> calculate_pi(A-1, B - 4 * (1 / (1 + A * 2)))
-    end.
+% @todo add guards
+% @todo rename useless params by _
+calculate_pi(LatestPi, NumberOfRounds, CurrentSign, Precision, IntSamePi, IntSamePi) ->
+IntSamePi / math:pow(10, Precision);
+calculate_pi(LatestPi, NumberOfRounds, CurrentSign, Precision, IntOldPi, IntLatestPi) ->
+NewPi = LatestPi + CurrentSign * 4 * (1 / (1 + NumberOfRounds * 2)),
+calculate_pi(NewPi, NumberOfRounds + 1, -1 * CurrentSign, Precision, IntLatestPi, trunc(NewPi * math:pow(10, Precision))).
