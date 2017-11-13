083230c75959e85c61ca033e29787db11b672d0d
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-13 23:46:07 +0000

    Replace unused parameters by _

--- pi.erl
+++ pi.erl
@@ -7,9 +7,8 @@ calculate_pi(0, 0, 1, Precision, null, 4).
     
 
 % @todo add guards
-% @todo rename useless params by _
-calculate_pi(LatestPi, NumberOfRounds, CurrentSign, Precision, IntSamePi, IntSamePi) ->
+calculate_pi(_, _, _, Precision, IntSamePi, IntSamePi) ->
 IntSamePi / math:pow(10, Precision);
-calculate_pi(LatestPi, NumberOfRounds, CurrentSign, Precision, IntOldPi, IntLatestPi) ->
+calculate_pi(LatestPi, NumberOfRounds, CurrentSign, Precision, _, IntLatestPi) ->
 NewPi = LatestPi + CurrentSign * 4 * (1 / (1 + NumberOfRounds * 2)),
 calculate_pi(NewPi, NumberOfRounds + 1, -1 * CurrentSign, Precision, IntLatestPi, trunc(NewPi * math:pow(10, Precision))).
