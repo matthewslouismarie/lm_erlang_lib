0e77f63009503cfe82991b62625a9613e097370d
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-14 00:06:50 +0000

    Replace NumberOfRounds by more accurate and simpler RoundNo

--- pi.erl
+++ pi.erl
@@ -14,13 +14,13 @@ calculate_pi(_, _, _, Precision, IntSamePi, IntSamePi) when
     Precision >= 0 ->
 IntSamePi / math:pow(10, Precision);
 
-calculate_pi(LatestPi, NumberOfRounds, CurrentSign, Precision, _, IntLatestPi) when
+calculate_pi(LatestPi, RoundNo, CurrentSign, Precision, _, IntLatestPi) when
     is_number(LatestPi),
-    is_integer(NumberOfRounds),
-    NumberOfRounds >= 0,
+    is_integer(RoundNo),
+    RoundNo >= 0,
     (CurrentSign =:= 1) or (CurrentSign =:= -1),
     is_integer(Precision),
     Precision >= 0,
     is_integer(IntLatestPi) ->
-NewPi = LatestPi + CurrentSign * 4 * (1 / (1 + NumberOfRounds * 2)),
-calculate_pi(NewPi, NumberOfRounds + 1, -1 * CurrentSign, Precision, IntLatestPi, trunc(NewPi * math:pow(10, Precision))).
+NewPi = LatestPi + CurrentSign * 4 * (1 / (1 + RoundNo * 2)),
+calculate_pi(NewPi, RoundNo + 1, -1 * CurrentSign, Precision, IntLatestPi, trunc(NewPi * math:pow(10, Precision))).
