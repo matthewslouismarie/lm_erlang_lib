021339d81497fff664196a0cf60019c5b534b725
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-30 23:52:58 +0000

    Merge individual line results

--- src/file_word_analysis.erl
+++ src/file_word_analysis.erl
@@ -10,11 +10,17 @@ analyse_file(Filename) ->
     end.
 
 process_file(Device) ->
-    read_one_line(file:read_line(Device), Device).
+    file:write_file("res.txt", io_lib:fwrite("~p.\n", [analyse_file(file:read_line(Device), Device, #{})])).
 
-read_one_line(eof, _) -> [];
-read_one_line({ok, Line}, Device) ->
-    analyse_line(Line, []).
+analyse_file(eof, _, Results) -> Results;
+analyse_file({ok, Line}, Device, Results) ->
+    NewResults = analyse_line(Line, []),
+    analyse_file(file:read_line(Device), Device, merge(Results, NewResults, maps:keys(NewResults))).
+
+merge(Results, #{}, []) -> Results;
+merge(Results, NewResults, [CurrentKey|NextKeys]) ->
+    {Value, Map} = maps:take(CurrentKey, NewResults),
+    merge(maps:put(CurrentKey, maps:get(CurrentKey, Results, 0) + 1, Results), Map, NextKeys).
 
 analyse_line(Line, Result) ->
     extract_words(Line, #{}, []).
