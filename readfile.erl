04d6e9a1079ddf1d5f71fd5ba4e1342e98a1d371
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-30 12:56:10 +0000

    Replace case statement by recursion

--- readfile.erl
+++ readfile.erl
@@ -1,5 +1,5 @@
 -module(readfile).
--export([readlines/1]). ()
+-export([readlines/1]).
 
 % from https://stackoverflow.com/a/2475613/7089212
 
@@ -9,8 +9,7 @@ readlines(FileName) ->
       after file:close(Device)
     end.
 
-get_all_lines(Device) ->
-    case io:get_line(Device, "") of
-        eof  -> [];
-        Line -> Line ++ get_all_lines(Device)
-    end.
+get_all_lines(Device) -> sub_get_all_lines(io:get_line(Device, ""), Device).
+
+sub_get_all_lines(eof, _) -> [];
+sub_get_all_lines(Line, Device) -> Line ++ get_all_lines(Device).
