276bd824c146ae7aab4c22b27147e1ddbcae5c44
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-30 12:53:00 +0000

    Create script to read files

---
+++ readfile.erl
@@ -0,0 +1,16 @@
+-module(readfile).
+-export([readlines/1]). ()
+
+% from https://stackoverflow.com/a/2475613/7089212
+
+readlines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    try get_all_lines(Device)
+      after file:close(Device)
+    end.
+
+get_all_lines(Device) ->
+    case io:get_line(Device, "") of
+        eof  -> [];
+        Line -> Line ++ get_all_lines(Device)
+    end.
