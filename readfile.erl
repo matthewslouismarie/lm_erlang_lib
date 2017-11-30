4377410dd5eb0e2c23e96ff0579a4be106f35870
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-30 13:13:29 +0000

    Use raw mode and file:read_line method

--- readfile.erl
+++ readfile.erl
@@ -4,10 +4,10 @@
 % from https://stackoverflow.com/a/2475613/7089212
 
 readlines(FileName) ->
-    {ok, Device} = file:open(FileName, [read]),
-    try get_all_lines(io:get_line(Device, ""), Device)
+    {ok, Device} = file:open(FileName, [raw, {read_ahead, 50}]),
+    try get_all_lines(file:read_line(Device), Device)
       after file:close(Device)
     end.
 
 get_all_lines(eof, _) -> [];
-get_all_lines(Line, Device) -> Line ++ get_all_lines(io:get_line(Device, ""), Device).
+get_all_lines({ok, Line}, Device) -> Line ++ get_all_lines(file:read_line(Device), Device).
