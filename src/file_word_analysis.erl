9f9fd7cb41efc399f5a9ab66f4ec50f1f9011ffc
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-30 17:01:36 +0000

    Remove commas from words

--- src/file_word_analysis.erl
+++ src/file_word_analysis.erl
@@ -19,8 +19,12 @@ read_one_line({ok, Line}, Device) ->
 analyse_line(Line, Result) ->
     extract_words(Line, [], []).
 
+% todo: use real characters
+% todo: prevent code duplication
+% todo: prevent empty slots
 extract_words([], ProcessedArray, CurrentWord) -> ProcessedArray;
-extract_words([32 | UnprocessedLine], ProcessedArray, CurrentWord) ->
+extract_words([NextCharacter | UnprocessedLine], ProcessedArray, CurrentWord)
+    when NextCharacter =:= 32; NextCharacter =:= 44 ->
     extract_words(UnprocessedLine, ProcessedArray ++ [CurrentWord], []);
 extract_words([NextCharacter | UnprocessedLine], ProcessedArray, CurrentWord) ->
     NewCurrentWord = CurrentWord ++ [NextCharacter],
