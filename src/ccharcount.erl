64541c9b3026ffba3ef0cefdd55e3f39cb40d607
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-12-02 18:07:54 +0000

    Apply minor changes

--- src/ccharcount.erl
+++ src/ccharcount.erl
@@ -8,12 +8,12 @@
 -export([load/1, count/3, go/2, join/2, split/2]).
 
 %% @spec load(Filename) -> list()
-%% @todo fix typo
+%% @todo string:to_lower is obsolete
 %% @doc Read a file and returns an analysis of its letter frequency.
 %% @param Filename The name of the file to analyze.
 %% @returns A list of tuples.
-load(F) ->
-    {ok, Bin} = file:read_file(F),
+load(Filename) ->
+    {ok, Bin} = file:read_file(Filename),
     List = binary_to_list(Bin),
     Length = round(length(List) / 20),
     Ls = string:to_lower(List),
